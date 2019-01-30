e0ba7fb501939d69aefdd27f50bfe5148291864d
DRooke <thodl15@gmail.com>
2019-01-30 09:49:50 -0500

    Add initial content for first main section and begin work on second.

--- src/components/views/WebMain.tsx
+++ src/components/views/WebMain.tsx
@@ -8,11 +8,35 @@
 
 import * as React from 'react';
 
+import './../../sass/main.css';
+
 const WebMain = () => {
 
     return (
         <div>
-            WebMain
+            <section className={"mainIntro"}>
+                <h1>
+                    Drive Less. Save More On Car Insurance.
+                </h1>
+                <h4>
+                    Car insurance based on how much you drive.
+                    Pay a low base rate then just pennies per mile.
+                </h4>
+                <form className={"zipInput"}>
+                    <input className={"col-xs-9 col-xl-3"} placeholder={"Enter Zip Code"}>
+                    </input>
+                    <button>
+                        Check Your Rate
+                    </button>
+                </form>
+
+                <h4>Call an agent at 1.888.242.5204</h4>
+            </section>
+            <section className={"mainTest"}>
+                <h2 className={"sectionHeader"}>
+                    Real People, Real Savings.
+                </h2>
+            </section>
         </div>
     )
 }
