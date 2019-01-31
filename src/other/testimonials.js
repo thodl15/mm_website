14ce182fdba868dcee5081012a1e46c693fba1ab
DRooke <thodl15@gmail.com>
2019-01-31 13:56:09 -0500

    Add JS file containing the user testimonials shown on the website.

---
+++ src/other/testimonials.js
@@ -0,0 +1,19 @@
+const testimonials = [
+    {
+        name: "Angela O",
+        rating: 5,
+        review: "This is the best move I have made in a long time. If you aren't on the road that much, you can save lots of money. I save between 50% to 70% every month."
+    },
+    {
+        name: "Bennie W",
+        rating: 5,
+        review: "I love Metromile. I went from paying $120 a month to $50 because I only drive 10 miles a day. Friendly people on the phone the two times I've had to call too."
+    },
+    {
+        name: "Riaz H",
+        rating: 5,
+        review: "Metromile is the perfect insurance company for the people who do not drive much. For the two cars that we have, it is around $45 a month."
+    }
+]
+
+export default testimonials;
