1d86652eaeda8553d8dc612aa45def66c2e21a5d
DRooke <thodl15@gmail.com>
2019-01-30 21:34:54 -0500

    Design and implement light and dark themes for the navigation bars.

--- src/components/core/WebHeader.tsx
+++ src/components/core/WebHeader.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import '../../sass/nav.css';
-import mmLogo from './../../img/logo-metromile-white.png';
-import burger from './../../img/hamburger_white.svg';
+import '../../sass/topNav.css';
 
 const WebHeader = () => {
     return (
         <div className={"nav-root-main"}>
             <div className={"col-sm-3 col-md-2 col-xs-4"}>
-                <a href={"#"} ><img src={mmLogo} className={"icon"} /></a>
+                <a href={"#"} className={"icon"} />
             </div>
             <div className={"col-sm-9 col-md-10 col-xs-8"}>
                 <div className={"nav-menu-options"}>
@@ -26,7 +24,7 @@ const WebHeader = () => {
                         </button>
                     </form>
                 </div>
-                <img src={burger} className={"menu-burger"} />
+                <div className={"menu-burger"} />
             </div>
         </div>
     )
--- src/components/core/WebHeaderFixed.tsx
+++ src/components/core/WebHeaderFixed.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 import WebHeader from './WebHeader';
 
-interface Props {
-    name?: string;
-}
+import './../../sass/fixedNav.css';
+
 
 interface State {
     isVisible: boolean;
 }
 
-class WebHeaderFixed extends React.Component<Props, State> {
-    constructor(props: Props) {
+const fixedStyle = {
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+    zIndex: 9000,
+    backgroundColor: '#fff',
+    color: '#0dc0c9',
+} as CSSProperties;
+
+class WebHeaderFixed extends React.Component<{}, State> {
+    constructor(props: any) {
         super(props);
         this.state = {
             isVisible: false
+        }
+
+        this.onScroll = this.onScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.onScroll);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener("scroll", this.onScroll);
+    }
+
+    onScroll(event : any) {
+        if((this.state.isVisible && window.scrollY < 150) || (!this.state.isVisible && window.scrollY >= 150)) {
+            this.setState((prevState : State, prevProps : any) => ({
+                isVisible: !prevState.isVisible
+            }));
         }
     }
 
     render() {
         return (
             this.state.isVisible ?
-                <WebHeader /> :
+                <div className={"fixedNav"}><WebHeader /></div> :
                 <div />
         )
     }
